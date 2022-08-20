7d3d8934ffb42e34f81ee145dd45169ca7aab2d8
JLemus89 <104263746+JLemus89@users.noreply.github.com>
2022-08-20 15:42:37 -0500

    cambio JL

--- FirstActivity/src/main/java/Tareas.java
+++ FirstActivity/src/main/java/Tareas.java
@@ -3,7 +3,7 @@ public class Tareas {
     public static void main (String args[]){
 
         System.out.println("Prueba numero 2 actualizar archivos");
-        System.out.println("cambio John Lemus");
+        System.out.println("cambio John Lemus v1");
 
     }
 }
