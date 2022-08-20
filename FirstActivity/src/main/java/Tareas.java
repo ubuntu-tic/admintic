058a867b3815a66e3723f88676143633b768f431
JLemus89 <104263746+JLemus89@users.noreply.github.com>
2022-08-20 17:15:02 -0500

    Delete FirstActivity directory

--- FirstActivity/src/main/java/Tareas.java
+++
@@ -1,9 +0,0 @@
-public class Tareas {
-
-    public static void main (String args[]){
-
-        System.out.println("Prueba numero 2 actualizar archivos");
-        System.out.println("cambio John Lemus v1");
-
-    }
-}
