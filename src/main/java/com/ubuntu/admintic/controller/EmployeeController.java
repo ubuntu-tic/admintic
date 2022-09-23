058cba722f3ae46c83a135679ebada7c82798550
yolfon <yolfon@gmail.com>
2022-09-23 18:27:15 -0400

    CRUD employee Usuarios

--- src/main/java/com/ubuntu/admintic/controller/EmployeeController.java
+++ src/main/java/com/ubuntu/admintic/controller/EmployeeController.java
@@ -2,22 +2,27 @@ package com.ubuntu.admintic.controller;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.ubuntu.admintic.entity.Employee;
+import com.ubuntu.admintic.entity.Enterprise;
 import com.ubuntu.admintic.entity.Profile;
 import com.ubuntu.admintic.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/Employee")
+
 public class EmployeeController {
 
-
+    @Autowired
     EmployeeService services;
 
+/*
     public EmployeeController(EmployeeService services) {
         this.services = services;
     }
@@ -48,6 +53,43 @@ public class EmployeeController {
     @PatchMapping("/employee/{id}")
     public String updateEmployee(@PathVariable int id){
         return "";
+    }*/
+
+    @GetMapping ("/usuarios")
+    private String verUsuario(Model model){
+        model.addAttribute("employees", services.verUsuario());
+        return "usuarios";
     }
+
+    @GetMapping("/nuevoUsuario")
+    private String verFormNuevoUsuario(Employee employee){
+        return "nuevoUsuario";
+    }
+
+    @PostMapping("usuarios")
+    private String crearUsuario(Employee employee){
+        services.crearYActualizarUsuario(employee);
+        return "redirect:/usuarios";
+    }
+
+    @GetMapping("/usuarios/borrar/{id}")
+    private String eliminarUsuario(@PathVariable("id") Long id){
+        services.eliminarUsuario(id);
+        return "redirect:/usuarios";
+    }
+
+    @GetMapping("usuarios/editar/{id}")
+    private String verUsuarioPorId(@PathVariable("id") Long id, Model model){
+        Employee employee = services.verUsuarioPorId(id);
+        model.addAttribute("employee", employee);
+        return "ActualizarUsuario";
+    }
+
+    @PostMapping("usuarios/actualizar/{id}")
+    private String editarUsuario(@PathVariable("id") Long id, Employee employee){
+        services.crearYActualizarUsuario(employee);
+        return "redirect:/usuarios";
+    }
+
 }
 
--- src/main/java/com/ubuntu/admintic/services/EmployeeService.java
+++ src/main/java/com/ubuntu/admintic/services/EmployeeService.java
@@ -2,19 +2,20 @@ package com.ubuntu.admintic.services;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.ubuntu.admintic.entity.Employee;
-
 import com.ubuntu.admintic.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EmployeeService {
-    private EmployeeRepository repository;
-    public EmployeeService(EmployeeRepository repository){
-        this.repository=repository;
-    }
 
+    @Autowired
+    private EmployeeRepository repository;
+
+
+    /*
     @JsonManagedReference
     public List<Employee> getEmployeeList(){
         return this.repository.findAll();
@@ -22,8 +23,23 @@ public class EmployeeService {
 
     public Employee createEmployee(Employee newEmployee){
         return this.repository.save(newEmployee);
+    } */
+
+    public void crearYActualizarUsuario(Employee employee){
+        repository.save(employee);
     }
-    
+
+    public List<Employee> verUsuario(){
+        return this.repository.findAll();
+    }
+
+    public Employee verUsuarioPorId(Long id){
+        return repository.findById(id).get();
+    }
+
+    public void eliminarUsuario(Long id){
+        repository.deleteById(id);
+    }
 
 }
 
