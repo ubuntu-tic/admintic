ada6dd2e0a4c5dc592651c7f51647ea2ca5ab113
JLemus89 <104263746+JLemus89@users.noreply.github.com>
2022-09-25 12:35:47 -0500

    actualización login

--- src/main/java/com/ubuntu/admintic/controller/FrontController.java
+++ src/main/java/com/ubuntu/admintic/controller/FrontController.java
@@ -1,5 +1,7 @@
 package com.ubuntu.admintic.controller;
 
+import com.ubuntu.admintic.entity.User;
+import com.ubuntu.admintic.services.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontController {
+    UserService userService;
 
 
     @GetMapping("/")
     public String index(Model model, @AuthenticationPrincipal OidcUser principal) {
+        if(principal !=null) {
+            model.addAttribute("user", principal.getClaims());
+        }
         return "index";
     }
 
